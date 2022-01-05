78a39c1e4329a3a3d7ad377f0bf2f8449b01e3e5
David Winter <dastw@gmx.net>
2022-01-05 18:57:04 +0100

    worker: Fix test configuration

--- worker/worker_test.py
+++ worker/worker_test.py
@@ -9,7 +9,8 @@ def run(config, debug):
     with open("test.sdfec.yml", "r") as f:
         code = worker.add_code(yaml.load(f))
 
-    task = SimulationTask(0, code, np.linspace(3, 7, (7-3)*4+1), snr_scales=np.ones(30)*2, term_time=10, term_errors=1e3, max_iterations=32)
+    points = (7-3)*4+1
+    task = SimulationTask(0, code, np.linspace(3, 7, points), snr_scales=np.ones(points)*2, term_time=10, term_errors=1e3, max_iterations=32)
     worker.submit_task(task)
 
     worker.join()
